38e66b54dcc7d21fd9814e99f1d9d8345f83e3f6
ArjunKvl <arjunaskvl@gmail.com>
2025-06-29 22:25:21 +0530

    added inbuilt exit and cd.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::io;
 use std::io::Write;
 use std::process::Command;
+use std::process;
 use std::str::SplitWhitespace;
+use std::path::Path;
+use std::env;
 
 fn main() {
     loop{
@@ -12,7 +15,15 @@ fn main() {
         let mut parts = input.trim().split_whitespace();
         let command = parts.next().unwrap().to_string();
         let args = parts;
-        execute(&command,args);
+        match command.as_str() {
+            "exit"=>process::exit(0x0100),
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                env::set_current_dir(&root).unwrap();
+            }
+            _=>execute(&command,args),
+        }
     }
 }
 
