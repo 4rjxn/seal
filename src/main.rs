209e26c2a25f333202ca4b3b53d0fdeca777ce0e
ArjunKvl <arjunaskvl@gmail.com>
2025-07-01 18:32:13 +0530

    need to implement a proper tokeniser

--- src/main.rs
+++ src/main.rs
@@ -36,18 +36,43 @@ impl RunEssentials{
 fn set_env()->RunEssentials{
     let essentials = RunEssentials::set_values();
     let path = Path::new(&essentials.home);
-    env::set_current_dir(&path).unwrap();
-    return essentials;
+    env::set_current_dir(path).unwrap();
+    essentials
+}
+
+fn tokenise(input:&String){
+    let chr_list = input.as_bytes();
+    let mut is_quote:bool = false;
+    let mut list:Vec<&str> = Vec::new();
+    let mut strbuffer:Vec<&str> = Vec::new();
+    let mut index = 0;
+    for (i,&val) in chr_list.iter().enumerate(){
+        if (val == b' ' || val == b'"') && !is_quote{
+            list.push(&input[index..i]);
+            index = i+1;
+        }
+        if (val == b' ' || val == b'"') && is_quote{
+            strbuffer.push(&input[index..i]);
+            index = i+1;
+        }
+        if val == b'"'{
+            is_quote = !is_quote;
+        }
+    }
+    println!("{:?}",list);
+    println!("{:?}",strbuffer.join(""));
 }
 
 fn main() {
     let user_details = set_env();
+    //println!("\"hai helloo ahd\"");
     loop{
         let mut input = String::new();
         print!("{}@{}:",user_details.user,user_details.host);
         io::stdout().flush().expect("flush error");
         io::stdin().read_line(&mut input).expect("read error");
-        let mut parts = input.trim().split_whitespace();
+        tokenise(&input);
+        let mut parts = input.split_whitespace();
         let user_input = UserInput{
             command:match parts.next(){
                 Some(rslt)=>rslt.to_string(),
@@ -60,7 +85,7 @@ fn main() {
             "cd" => {
                 let new_dir = user_input.args.peekable().peek().map_or("/", |x| *x);
                 let root = Path::new(new_dir);
-                env::set_current_dir(&root).unwrap();
+                env::set_current_dir(root).unwrap();
             }
             _=>execute(user_input),
         }
